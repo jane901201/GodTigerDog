8146f6afcf807cf35c039c2f9ed63e03733bc3d8
jane901201 <jane36742715@gmail.com>
2021-10-28 12:27:25 +0800

    add value in MainActivity

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val edName = null;
+    val tvText = null;
+    val btnScissor = null;
+    val btnStone = null;
+    val btnPaper = null;
+    val btnMora = null;
+    val tvName = null;
+    val tvWinner = null;
+    val tvMora = null;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
